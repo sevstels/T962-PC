e79f687e0a369ca481a1e4da7d9a279cb3598a37
sevstels <decoder@rambler.ru>
2022-11-15 17:55:48 +0800

    -

--- src/Version/firmware_build.h
+++ src/Version/firmware_build.h
@@ -7,8 +7,8 @@
 #ifndef _FIRMWARE_BUILD_DATA_H_
 #define _FIRMWARE_BUILD_DATA_H_
 
-//Last build data Year: 2000 + 22, Month: 11, Day: 15, Hour: 17:50
-#define FIRMWARE_BUILD_DATA       2211151750
-#define FIRMWARE_BUILD_DATA_STR  "2022.11.15 17:50"
+//Last build data Year: 2000 + 22, Month: 11, Day: 15, Hour: 17:55
+#define FIRMWARE_BUILD_DATA       2211151755
+#define FIRMWARE_BUILD_DATA_STR  "2022.11.15 17:55"
 
 #endif
